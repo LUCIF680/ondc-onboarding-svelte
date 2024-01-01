834e0fc37a7cb6a38c83f770e29a4146907a29b9
pratik.mazumdar <pratik@WILASSETA0188.local>
2024-01-01 11:36:43 +0530

    initial commit

---
+++ src/lib/UI/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import NextArrow from '$lib/svg/NextArrow.svg.svelte';
+
+	export let content: string;
+	export let href: string;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	on:click={() => (document.location.href = href)}
+	class="bg-blue-500 w-1/2 text-white py-3 m-5 px-4 rounded-3xl hover:bg-blue-600 hover:shadow-lg cursor-pointer"
+>
+	<div class="flex">
+		<span>{content}</span>
+		<NextArrow />
+	</div>
+</div>
