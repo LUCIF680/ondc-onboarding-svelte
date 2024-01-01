5cfad0c1467fe29b83ab63eec70647cfa05edc02
lucif680 <pratkmazumdar680@protonmail.com>
2024-01-01 11:50:20 +0530

    v0.0.1

---
+++ src/lib/UI/Progressbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let width: number = 0;
+	import { onMount } from 'svelte';
+	onMount(() => {
+		window.onscroll = () => {
+			if (document.documentElement.scrollTop < document.body.offsetHeight) {
+				width =
+					(document.documentElement.scrollTop / (document.body.offsetHeight - window.innerHeight)) *
+					100;
+			}
+		};
+	});
+</script>
+
+<div class="w-full bg-gray-200 rounded-full h-1.5 mb-4 z-10 fixed">
+	<div class="bg-blue-500 h-1.5 rounded-full" style="width: {width}%"></div>
+</div>
---
+++ src/lib/svg/NextArrow.svg.svelte
@@ -0,0 +1,11 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	fill="currentColor"
+	class="m-1 ml-2"
+	height="16"
+	width="16"
+	><path fill="none" d="M0 0h24v24H0z"></path><path
+		d="m15 5-1.41 1.41L18.17 11H2v2h16.17l-4.59 4.59L15 19l7-7-7-7z"
+	></path>
+</svg>
